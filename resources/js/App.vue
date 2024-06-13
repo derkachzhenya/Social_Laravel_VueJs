ced02677fb7d8ef215de5b715be3cf163a617c1e
zhenya457 <derkachzhenya>
2024-06-13 13:09:57 +0300

    l1

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-4xl font-bold underline text-center mb-5 mt-5">
+    Hello world!
+  </h1>
+  <div><router-link to="index">Index</router-link></div>
+  <div><router-link to="page">Page</router-link></div>
+  
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+
+<style>
+</style>
---
+++ resources/js/views/Page.vue
@@ -0,0 +1,6 @@
+<template>
+   <h1>Page</h1>
+</template>
+
+
+<script ></script>
